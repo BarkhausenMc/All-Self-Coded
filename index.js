e6f0416e08b3a37fbe4e0705beb382725c12ac52
BarkhausenMc <293185011+BarkhausenMc@users.noreply.github.com>
2026-08-13 12:21:59 +0200

    Aktualisieren von index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 
 const client = new Client({
@@ -10,10 +10,34 @@ const client = new Client({
 client.once('ready', async () => {
     console.log('Bot ist online!');
 
-    const channel = client.channels.cache.get('1537389571103522868');
+    const channel = client.channels.cache(1537389571103522868);
 
     if (channel) {
-        await channel.send('```Hallo von meinem Bot!```');
+        // --- Embed erstellen ---
+        const embed = new EmbedBuilder()
+            .setColor(0x6d4aff)                    // Lila Streifen links
+            .setTitle('Willkommen auf dem Server!')
+            .setURL('https://example.com')          // Titel wird klickbar
+            .setAuthor({ 
+                name: 'Bot Name',
+                iconURL: 'https://example.com/icon.png'
+            })
+            .setDescription('Das ist die **Beschreibung**.\nDu kannst hier *Markdown* nutzen.')
+            .setThumbnail('https://example.com/thumbnail.png')
+            .addFields(
+                { name: 'Spieler online', value: '42', inline: true },
+                { name: 'Server-Region', value: 'EU-West', inline: true },
+                { name: 'Nächstes Event', value: 'Heute 20 Uhr', inline: false }
+            )
+            .setImage('https://example.com/banner.png')
+            .setTimestamp()
+            .setFooter({ 
+                text: 'Powered by Flo',
+                iconURL: 'https://example.com/footer-icon.png'
+            });
+
+        // --- Embed senden ---
+        channel.send({ embeds: [embed] });
     } else {
         console.log('Channel nicht gefunden!');
     }
