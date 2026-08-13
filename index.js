9c066412007c7ad37a54f00bdc71baec8f973a48
BarkhausenMc <293185011+BarkhausenMc@users.noreply.github.com>
2026-08-13 11:16:29 +0200

    index.js erstellen

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
+});
+
+client.once('ready', async () => {
+    console.log('Bot ist online!');
+
+    const channel = client.channels.cache.get('DEINE_CHANNEL_ID_HIER');
+
+    if (channel) {
+        await channel.send('Hallo von meinem Bot!');
+    } else {
+        console.log('Channel nicht gefunden!');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
