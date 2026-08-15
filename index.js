4468d481fed1b8c25d8041448d60f7c7573d6881
Flo <you@example.com>
2026-08-15 21:53:06 +0200

    9

--- index.js
+++ index.js
@@ -58,27 +58,26 @@ client.once('ready', async () => {
                 new SeparatorBuilder().setDivider(true).setSpacing(1)
             )
             .addTextDisplayComponents(
-                new TextDisplayBuilder().setContent('Klicke unten auf den `💰 VERKAUFEN` oder `🛒 ANKAUF` Button, um einen Trade zu Starten.\nWir versuchen immer faire und optimale Preise anzubieten.')
+                new TextDisplayBuilder().setContent('Klicke unten auf den `💰 VERKAUFEN` oder `🛒 ANKAUF` Button, um einen Trade zu Starten.')
             )
 
-            .addActionRowComponents(
-    new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-            .setCustomId('spawner_verkaufen')
-            .setLabel('VERKAUFEN')
-            .setStyle(ButtonStyle.Success)
-            .setEmoji('💰'),
-        new ButtonBuilder()
-            .setCustomId('spawner_ankaufen')
-            .setLabel('ANKAUF')
-            .setStyle(ButtonStyle.Primary)
-            .setEmoji('🛒')
-    )
-)
-        channel.send({
-            components: [container],
-            flags: MessageFlags.IsComponentsV2
-        });
+            const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+        .setCustomId('spawner_verkaufen')
+        .setLabel('VERKAUFEN')
+        .setStyle(ButtonStyle.Success)
+        .setEmoji('💰'),
+    new ButtonBuilder()
+        .setCustomId('spawner_ankaufen')
+        .setLabel('ANKAUF')
+        .setStyle(ButtonStyle.Primary)
+        .setEmoji('🛒')
+);
+
+channel.send({
+    components: [container, row],
+    flags: MessageFlags.IsComponentsV2
+});
     } else {
         console.log('Channel nicht gefunden!');
     }
