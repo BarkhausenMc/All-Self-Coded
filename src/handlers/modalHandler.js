fa54477decba239802386d5e595f8096c7a83308
Flo <you@example.com>
2026-08-16 22:14:51 +0200

    jz fertig

--- src/handlers/modalHandler.js
+++ src/handlers/modalHandler.js
@@ -42,7 +42,7 @@ module.exports = async function handleModal(interaction) {
         const totalPrice = pricePerUnit * parsedAmount;
 
         const thread = await interaction.channel.threads.create({
-            name: `${emoji} ${action} - ${ingameName} - Stk.:${amount}`,
+            name: `${emoji} ${action} - ${ingameName} - 📦 Menge: ${amount}`,
             type: ChannelType.PrivateThread,
             invitable: false
         });
