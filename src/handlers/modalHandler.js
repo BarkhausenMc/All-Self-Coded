e7b45b1a2f15ed68c6196a03bf8254f8cd7428bb
Flo <you@example.com>
2026-08-16 22:16:06 +0200

    mit spawner typ

--- src/handlers/modalHandler.js
+++ src/handlers/modalHandler.js
@@ -42,7 +42,7 @@ module.exports = async function handleModal(interaction) {
         const totalPrice = pricePerUnit * parsedAmount;
 
         const thread = await interaction.channel.threads.create({
-            name: `${emoji} ${action} - ${ingameName} - 📦 Menge: ${amount}`,
+            name: `${emoji} ${action} - ${ingameName} - 📦 Menge: ${amount} - ${spawnerType}`,
             type: ChannelType.PrivateThread,
             invitable: false
         });
