91851c07229bab1c354bf121eccbc888537c921c
Flo <you@example.com>
2026-08-16 22:12:43 +0200

    + amount im ticket namen

--- src/handlers/modalHandler.js
+++ src/handlers/modalHandler.js
@@ -42,7 +42,7 @@ module.exports = async function handleModal(interaction) {
         const totalPrice = pricePerUnit * parsedAmount;
 
         const thread = await interaction.channel.threads.create({
-            name: `${emoji} ${action} - ${ingameName}`,
+            name: `${emoji} ${action} - ${ingameName} - ${amount}`,
             type: ChannelType.PrivateThread,
             invitable: false
         });
