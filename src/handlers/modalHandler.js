f2a4e7b1d6ddec31f16a41e74d4b74b93a8b9c19
Flo <you@example.com>
2026-08-16 22:18:58 +0200

    jz richt emoji von spawner

--- src/handlers/modalHandler.js
+++ src/handlers/modalHandler.js
@@ -42,7 +42,7 @@ module.exports = async function handleModal(interaction) {
         const totalPrice = pricePerUnit * parsedAmount;
 
         const thread = await interaction.channel.threads.create({
-            name: `${emoji} ${action} - ${constants.spawnerEmojis} ${spawnerType} - 📦 Menge: ${amount} `,
+            name: `${emoji} ${action} - ${spawnerEmoji} ${spawnerType} - 📦 Menge: ${amount} `,
             type: ChannelType.PrivateThread,
             invitable: false
         });
