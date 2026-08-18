2f9d722efe3fc31d42482cd551ee88dbf64c8cbc
Flo <you@example.com>
2026-08-18 22:44:19 +0200

    jz fr

--- src/commands/traderAdmin/announce.js
+++ src/commands/traderAdmin/announce.js
@@ -80,7 +80,7 @@ module.exports = {
             .addTextDisplayComponents(
                 new TextDisplayBuilder().setContent(
                     `📋 Unsere **Aktuelle Preise** siehe Trading Panel.☝️\n\n` +
-                    `> ||* Geändert von <@${interaction.user.id}> *||` +
+                    `> ||* Geändert von <@${interaction.user.id}> *||\n` +
                     `||* ${rolePing} *||`
                 )
             );
